5ecf2c9008b4e1c022318ac9828a1cf949388767
wDaniec <andrzejZdobywca@gmail.com>
2021-02-11 16:38:55 +0100

    Add pytorch lightning module

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from src.models import SimpleCNN
 from src.data import fmnist
+from src.modules import Classifier
 
 def main():
     model = SimpleCNN(5, 32)
     train_loader, valid_loader = fmnist()
+    pl_module = Classifier(model)
     print(model)
     print(train_loader, valid_loader)
+    print(pl_module)
 
 if __name__ == "__main__":
     main()
