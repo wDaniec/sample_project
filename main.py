cbb9d8616cfd437b68f0a203252f06401463a638
wDaniec <andrzejZdobywca@gmail.com>
2021-02-11 21:41:21 +0100

    Add resnet18

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from src.models import SimpleCNN
+from src.models import ResNet18
 from src.data import cifar
 from src.modules import Classifier
 import pytorch_lightning as pl
 
 
 def main():
-    model = SimpleCNN(100)
+    model = ResNet18(100)
     train_loader, valid_loader = cifar(variant="100")
     pl_module = Classifier(model)
     trainer = pl.Trainer(gpus=1, max_epochs=20)
